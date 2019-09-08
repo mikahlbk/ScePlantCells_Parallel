1905b2924b7624945f4d401ee846900c796d0749
Mikahl Banwarth-Kuhn <mbanw001@pelican.ib.hpcc.ucr.edu>
2019-09-08 11:50:42 -0700

    September 7

--- SBATCH_SCRIPT.sh
+++ SBATCH_SCRIPT.sh
@@ -3,15 +3,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=2G
-#SBATCH --time=01:00:00
-#SBATCH --output=test_mech_div2.stdout
-#SBATCH --job-name="test_mech_div2"
-#SBATCH -p short
+#SBATCH --time=32:00:00
+#SBATCH --output=test_chemical_tissue_2.stdout
+#SBATCH --job-name="test_chemical_tissue_2"
+#SBATCH -p batch
 
 set OMP_NUM_THREADS=12
-mkdir Animate_cyt_mech_div2
-mkdir Locations_test_mech_div
-mkdir Locations_test_mech_div_2
-mkdir Animate_No_cyt_mech_div
-./program Animate_cyt_mech_div2 Locations_test_mech_div Locations_test_mech_div_2 Animate_No_cyt_mech_div
+mkdir Animate_chemical_tissue_2
+mkdir Locations_chemical_tissue_2
+mkdir Locations_chemical_NC_tissue_2
+mkdir Animate_chemical_NC_tissue_2
+./program Animate_chemical_tissue_2 Animate_chemical_NC_tissue_2 Locations_chemical_tissue_2 Locations_chemical_NC_tissue_2
                     
--- rand.cpp
+++ rand.cpp
@@ -15,6 +15,4 @@ double unifRand() {
 double unifRand(double a, double b) {
 	return (b-a)*unifRand() + a;
 }
-void seed() {
-	srand(time(0));
-}
+
